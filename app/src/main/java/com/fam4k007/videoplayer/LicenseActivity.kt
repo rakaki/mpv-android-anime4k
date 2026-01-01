17a0f8c7517a65a25f90b58df393b88869364242
azxcvn <2735059195@qq.com>
2026-01-01 21:08:28 +0800

    1 优化开源许可界面

--- app/src/main/java/com/fam4k007/videoplayer/LicenseActivity.kt
+++ app/src/main/java/com/fam4k007/videoplayer/LicenseActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
-import com.fam4k007.videoplayer.compose.LicenseItem
 import com.fam4k007.videoplayer.compose.LicenseScreen
 import com.fam4k007.videoplayer.ui.theme.getThemeColors
 import com.fam4k007.videoplayer.utils.ThemeManager
 
 /**
- * 许可证书页面
+ * 许可证书页面 - 使用 AboutLibraries 自动化管理
  */
 class LicenseActivity : BaseActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val licenseItems = buildLicenseItems()
         val activity = this
 
         setContent {
@@ -36,7 +34,6 @@ class LicenseActivity : BaseActivity() {
                 )
             ) {
                 LicenseScreen(
-                    licenses = licenseItems,
                     onBack = {
                         activity.finish()
                         activity.overridePendingTransition(R.anim.no_anim, R.anim.slide_out_down)
@@ -44,191 +41,5 @@ class LicenseActivity : BaseActivity() {
                 )
             }
         }
-    }
-    
-    private fun buildLicenseItems(): List<LicenseItem> {
-        return listOf(
-            LicenseItem(
-                name = "MPV Player",
-                licenseTag = "GPL-2.0",
-                summary = "跨平台高性能媒体播放器内核，负责解码与渲染。",
-                website = "https://mpv.io/",
-                licenseText = getMpvLicense()
-            ),
-            LicenseItem(
-                name = "Anime4K",
-                licenseTag = "MIT",
-                summary = "实时动漫超分辨率算法，用于提升画质。",
-                website = "https://github.com/bloc97/Anime4K",
-                licenseText = getMitLicense()
-            ),
-            LicenseItem(
-                name = "DanmakuFlameMaster",
-                licenseTag = "Apache-2.0",
-                summary = "哔哩哔哩开源弹幕引擎，支撑弹幕显示功能。",
-                website = "https://github.com/bilibili/DanmakuFlameMaster",
-                licenseText = getApacheLicense()
-            ),
-            LicenseItem(
-                name = "Glide",
-                licenseTag = "BSD/Apache-2.0",
-                summary = "图片加载与缓存库，用于封面与缩略图。",
-                website = "https://github.com/bumptech/glide",
-                licenseText = getGlideLicense()
-            ),
-            LicenseItem(
-                name = "AndroidX & Material",
-                licenseTag = "Apache-2.0",
-                summary = "现代化 Android UI 组件与官方支持库。",
-                website = "https://developer.android.com/jetpack/androidx",
-                licenseText = getApacheLicense()
-            ),
-            LicenseItem(
-                name = "Kotlin",
-                licenseTag = "Apache-2.0",
-                summary = "JetBrains 出品的现代语言，是应用主要开发语言。",
-                website = "https://kotlinlang.org/",
-                licenseText = getApacheLicense()
-            ),
-            LicenseItem(
-                name = "mpv-android",
-                licenseTag = "MIT",
-                summary = "mpv 在 Android 平台的移植项目，提供底层接口。",
-                website = "https://github.com/mpv-android/mpv-android",
-                licenseText = getMpvAndroidLicense()
-            ),
-            LicenseItem(
-                name = "mpvKt",
-                licenseTag = "Apache-2.0",
-                summary = "为 mpv 提供 Kotlin 封装的实用库。",
-                website = "https://github.com/L0uckY/mpvKt",
-                licenseText = getApacheLicense()
-            ),
-            LicenseItem(
-                name = "DanDanPlayForAndroid",
-                licenseTag = "Apache-2.0",
-                summary = "哔哩哔哩番剧数据与弹幕下载参考实现。",
-                website = "https://github.com/xyoye/DanDanPlayForAndroid",
-                licenseText = getApacheLicense()
-            )
-        )
-    }
-    
-    private fun getMpvLicense(): String {
-        return """
-GNU GENERAL PUBLIC LICENSE
-Version 2, June 1991
-
-Copyright (C) 1989, 1991 Free Software Foundation, Inc.
-51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-
-Everyone is permitted to copy and distribute verbatim copies
-of this license document, but changing it is not allowed.
-
-Preamble
-
-The licenses for most software are designed to take away your freedom to share and change it. By contrast, the GNU General Public License is intended to guarantee your freedom to share and change free software--to make sure the software is free for all its users.
-
-This General Public License applies to most of the Free Software Foundation's software and to any other program whose authors commit to using it.
-
-When we speak of free software, we are referring to freedom, not price. Our General Public Licenses are designed to make sure that you have the freedom to distribute copies of free software (and charge for this service if you wish), that you receive source code or can get it if you want it, that you can change the software or use pieces of it in new free programs; and that you know you can do these things.
-
-完整许可证请访问: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-        """.trimIndent()
-    }
-    
-    private fun getMitLicense(): String {
-        return """
-MIT License
-
-Copyright (c) 2019 bloc97
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-        """.trimIndent()
-    }
-    
-    private fun getApacheLicense(): String {
-        return """
-Apache License
-Version 2.0, January 2004
-http://www.apache.org/licenses/
-
-TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
-
-1. Definitions.
-
-"License" shall mean the terms and conditions for use, reproduction, and distribution as defined by Sections 1 through 9 of this document.
-
-"Licensor" shall mean the copyright owner or entity authorized by the copyright owner that is granting the License.
-
-"You" (or "Your") shall mean an individual or Legal Entity exercising permissions granted by this License.
-
-2. Grant of Copyright License. Subject to the terms and conditions of this License, each Contributor hereby grants to You a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable copyright license to reproduce, prepare Derivative Works of, publicly display, publicly perform, sublicense, and distribute the Work and such Derivative Works in Source or Object form.
-
-3. Grant of Patent License. Subject to the terms and conditions of this License, each Contributor hereby grants to You a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable patent license to make, have made, use, offer to sell, sell, import, and otherwise transfer the Work.
-
-4. Redistribution. You may reproduce and distribute copies of the Work or Derivative Works thereof in any medium, with or without modifications, provided that You meet the following conditions:
-   - You must give any other recipients of the Work a copy of this License
-   - You must retain all copyright, patent, trademark notices
-   - If the Work includes a "NOTICE" text file, you must include a copy
-
-完整许可证请访问: http://www.apache.org/licenses/LICENSE-2.0
-        """.trimIndent()
-    }
-    
-    private fun getGlideLicense(): String {
-        return """
-License for Glide
-
-Glide is licensed under the BSD License and the Apache License 2.0.
-
-BSD License:
-Copyright 2014 Google, Inc. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-3. Neither the name of Google Inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED.
-
-完整许可证请访问: https://github.com/bumptech/glide/blob/master/LICENSE
-        """.trimIndent()
-    }
-    
-    private fun getMpvAndroidLicense(): String {
-        return """
-MIT License
-
-Copyright (c) 2016 Ilya Zhuravlev
-Copyright (c) 2016 sfan5 <sfan5@live.de>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-完整许可证请访问: https://github.com/mpv-android/mpv-android/blob/master/LICENSE
-        """.trimIndent()
     }
 }
--- app/src/main/java/com/fam4k007/videoplayer/compose/LicenseScreen.kt
+++ app/src/main/java/com/fam4k007/videoplayer/compose/LicenseScreen.kt
@@ -1,69 +1,30 @@
 package com.fam4k007.videoplayer.compose
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Link
-import androidx.compose.material.icons.filled.TextSnippet
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.AssistChipDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
+import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
 import com.fam4k007.videoplayer.compose.SettingsColors as SettingsPalette
 
 /**
- * Compose 许可证书列表界面。
+ * 许可证书界面 - 使用 AboutLibraries 自动化管理
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LicenseScreen(
-    licenses: List<LicenseItem>,
     onBack: () -> Unit
 ) {
-    var selectedLicense by remember { mutableStateOf<LicenseItem?>(null) }
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     Scaffold(
@@ -72,7 +33,7 @@ fun LicenseScreen(
             .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             TopAppBar(
-                title = { Text("许可证书", fontSize = 18.sp, fontWeight = FontWeight.Bold) },
+                title = { Text("开源许可", fontSize = 18.sp, fontWeight = FontWeight.Bold) },
                 navigationIcon = {
                     IconButton(onClick = onBack) {
                         Icon(Icons.Default.ArrowBack, contentDescription = "返回")
@@ -87,191 +48,57 @@ fun LicenseScreen(
             )
         }
     ) { padding ->
-        LazyColumn(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(SettingsPalette.ScreenBackground)
                 .padding(padding)
-                .padding(horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            contentPadding = PaddingValues(top = 16.dp, bottom = 16.dp)
         ) {
-            items(licenses) { license ->
-                LicenseCard(license = license) { selectedLicense = license }
-            }
-
-            item { Spacer(Modifier.height(16.dp)) }
-        }
-    }
-
-    selectedLicense?.let { license ->
-        LicenseDetailDialog(
-            license = license,
-            onDismiss = { selectedLicense = null }
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun LicenseCard(
-    license: LicenseItem,
-    onClick: () -> Unit
-) {
-    val primaryColor = MaterialTheme.colorScheme.primary
-
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(16.dp),
-        colors = CardDefaults.cardColors(containerColor = SettingsPalette.CardBackground),
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-        onClick = onClick
-    ) {
-        Column(modifier = Modifier.padding(20.dp)) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = license.name,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    color = SettingsPalette.PrimaryText,
-                    modifier = Modifier.weight(1f)
-                )
-
-                AssistChip(
-                    onClick = onClick,
-                    label = { Text(license.licenseTag) },
-                    colors = AssistChipDefaults.assistChipColors(
-                        containerColor = SettingsPalette.Highlight,
-                        labelColor = SettingsPalette.AccentText
-                    )
-                )
-            }
-
-            Spacer(Modifier.height(8.dp))
-
-            Text(
-                text = license.summary,
-                fontSize = 14.sp,
-                color = SettingsPalette.SecondaryText,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-
-            Spacer(Modifier.height(12.dp))
-
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(
-                    imageVector = Icons.Default.Link,
-                    contentDescription = null,
-                    tint = primaryColor,
-                    modifier = Modifier.padding(end = 6.dp)
-                )
-                Text(
-                    text = license.website,
-                    fontSize = 13.sp,
-                    color = SettingsPalette.AccentText,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-            }
-        }
-    }
-}
-
-@Composable
-private fun LicenseDetailDialog(
-    license: LicenseItem,
-    onDismiss: () -> Unit
-) {
-    val uriHandler = LocalUriHandler.current
-    val primaryColor = MaterialTheme.colorScheme.primary
-
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        confirmButton = {
-            TextButton(onClick = onDismiss) {
-                Text("关闭", color = SettingsPalette.AccentText)
-            }
-        },
-        title = {
-            Column {
-                Text(license.name, fontWeight = FontWeight.Bold, fontSize = 18.sp, color = SettingsPalette.PrimaryText)
-                Text(license.licenseTag, fontSize = 13.sp, color = SettingsPalette.SecondaryText)
-            }
-        },
-        text = {
-            Column {
-                Box(
+            // 统计信息卡片
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = SettingsPalette.CardBackground
+                ),
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+            ) {
+                Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .clip(RoundedCornerShape(12.dp))
-                        .background(SettingsPalette.Highlight)
-                        .padding(12.dp)
-                ) {
-                    Column {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Icon(
-                                imageVector = Icons.Default.Link,
-                                contentDescription = null,
-                                tint = primaryColor
-                            )
-                            Spacer(Modifier.width(8.dp))
-                            Text(
-                                text = license.website,
-                                color = SettingsPalette.AccentText,
-                                modifier = Modifier.weight(1f)
-                            )
-                            IconButton(onClick = { uriHandler.openUri(license.website) }) {
-                                Icon(
-                                    imageVector = Icons.Default.TextSnippet,
-                                    contentDescription = "打开官网",
-                                    tint = primaryColor
-                                )
-                            }
-                        }
-                    }
-                }
-
-                Spacer(Modifier.height(12.dp))
-
-                Text(
-                    text = "许可证全文",
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    color = SettingsPalette.PrimaryText
-                )
-
-                Spacer(Modifier.height(8.dp))
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .heightIn(max = 320.dp)
-                        .clip(RoundedCornerShape(12.dp))
-                        .background(SettingsPalette.DialogSurface)
-                        .padding(12.dp)
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
-                        text = license.licenseText,
+                        text = "📦 感谢开源社区",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = SettingsPalette.PrimaryText
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "本应用使用了多个优秀的开源库",
                         fontSize = 13.sp,
-                        color = SettingsPalette.SecondaryText,
-                        modifier = Modifier.verticalScroll(rememberScrollState())
+                        color = SettingsPalette.SecondaryText
                     )
                 }
             }
-        },
-        shape = RoundedCornerShape(20.dp),
-        containerColor = SettingsPalette.DialogBackground
-    )
+            
+            // AboutLibraries 自动生成的列表
+            LibrariesContainer(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                colors = LibraryDefaults.libraryColors(
+                    backgroundColor = SettingsPalette.ScreenBackground,
+                    contentColor = SettingsPalette.PrimaryText,
+                    badgeBackgroundColor = MaterialTheme.colorScheme.primary,
+                    badgeContentColor = MaterialTheme.colorScheme.onPrimary,
+                    dialogConfirmButtonColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    }
 }
-
-/**
- * 许可证展示数据。
- */
-data class LicenseItem(
-    val name: String,
-    val licenseTag: String,
-    val summary: String,
-    val website: String,
-    val licenseText: String
-)
